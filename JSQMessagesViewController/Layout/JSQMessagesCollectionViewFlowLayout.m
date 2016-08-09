5f0a970ed105ae7a3ae7de24cfd0624271d1dcb4
Danan Arief Desprianto <dananarief@gmail.com>
2016-08-09 15:15:32 +0700

    Fix Bubble Calculator for two items that received at same time

--- JSQMessagesViewController/Layout/JSQMessagesCollectionViewFlowLayout.m
+++ JSQMessagesViewController/Layout/JSQMessagesCollectionViewFlowLayout.m
@@ -220,10 +220,8 @@ const CGFloat kJSQMessagesCollectionViewAvatarSizeDefault = 30.0f;
 
 - (id<JSQMessagesBubbleSizeCalculating>)bubbleSizeCalculator
 {
-    if (_bubbleSizeCalculator == nil) {
-        _bubbleSizeCalculator = [JSQMessagesBubblesSizeCalculator new];
-    }
-
+    _bubbleSizeCalculator = [JSQMessagesBubblesSizeCalculator new];
+    
     return _bubbleSizeCalculator;
 }
 
@@ -406,7 +404,7 @@ const CGFloat kJSQMessagesCollectionViewAvatarSizeDefault = 30.0f;
 {
     id<JSQMessageData> messageItem = [self.collectionView.dataSource collectionView:self.collectionView
                                                       messageDataForItemAtIndexPath:indexPath];
-
+    
     return [self.bubbleSizeCalculator messageBubbleSizeForMessageData:messageItem
                                                           atIndexPath:indexPath
                                                            withLayout:self];
